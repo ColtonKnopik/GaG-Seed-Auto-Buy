de1e974a049aecd7d238ce705a2cdb2523ac2aa9
Colton Knopik <120224879+ColtonKnopik@users.noreply.github.com>
2025-08-01 19:47:20 -0700

    fixed restock listener

---
+++ restock.py
@@ -0,0 +1,39 @@
+import pyautogui
+import time
+
+#Restock popup position and color
+restock_positions = [
+    (520, 202),
+    (520, 224)
+]
+restock_colors = [
+    (158, 197, 210),
+    (171, 213, 227),
+    (144, 238, 144)
+]
+
+null_colors = [
+    (84, 172, 59),
+    (17, 85, 12)
+]
+
+def check_restock_pos(restock_pos):
+    color = pyautogui.pixel(*restock_pos)
+
+    if color in restock_colors:
+        return True
+    
+    else:
+        return False
+
+
+def restock_listener():
+    print("Listening for restock...")
+    time.sleep(2)
+    while True:
+        for restock_pos in restock_positions:
+            if check_restock_pos(restock_pos):
+                print(f"Restock detected at {restock_pos}!")
+                return True
+        time.sleep(1)  # Check every second
+
